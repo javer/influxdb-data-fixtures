cf38a7f125baf266660f018720d4ce89fb61f202
yariknechyporuk <yarik.nechyporuk@gmail.com>
2023-01-27 17:43:26 +0800

    Version 2
    * drop purge type
    * use new odm

--- Purger/MeasurementPurger.php
+++ Purger/MeasurementPurger.php
@@ -6,28 +6,12 @@ use Doctrine\Common\DataFixtures\Purger\PurgerInterface;
 use Javer\InfluxDB\ODM\Mapping\ClassMetadata;
 use Javer\InfluxDB\ODM\MeasurementManager;
 
-class MeasurementPurger implements PurgerInterface
+final class MeasurementPurger implements PurgerInterface
 {
-    public const PURGE_MODE_DELETE   = 1;
-    public const PURGE_MODE_TRUNCATE = 2;
-
-    private MeasurementManager $measurementManager;
-
-    private int $purgeMode = self::PURGE_MODE_TRUNCATE;
-
-    public function __construct(MeasurementManager $measurementManager)
+    public function __construct(
+        private MeasurementManager $measurementManager,
+    )
     {
-        $this->measurementManager = $measurementManager;
-    }
-
-    public function setPurgeMode(int $mode): void
-    {
-        $this->purgeMode = $mode;
-    }
-
-    public function getPurgeMode(): int
-    {
-        return $this->purgeMode;
     }
 
     public function setMeasurementManager(MeasurementManager $measurementManager): void
@@ -42,16 +26,10 @@ class MeasurementPurger implements PurgerInterface
 
     public function purge(): void
     {
-        $database = $this->measurementManager->getDatabase();
-
         foreach ($this->measurementManager->getMetadataFactory()->getAllMetadata() as $metadata) {
             assert($metadata instanceof ClassMetadata);
 
-            if ($this->getPurgeMode() === self::PURGE_MODE_DELETE) {
-                $database->query(sprintf('DELETE FROM "%s"', $metadata->getMeasurement()));
-            } else {
-                $database->query(sprintf('DROP MEASUREMENT "%s"', $metadata->getMeasurement()));
-            }
+            $this->measurementManager->getClient()->dropMeasurement($metadata->getMeasurement());
         }
     }
 }
